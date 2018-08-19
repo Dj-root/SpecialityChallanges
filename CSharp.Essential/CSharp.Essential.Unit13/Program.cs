0bdb5ffc703d583039b1623a9e3854a05172209e
holms <holms.html@gmail.com>
2018-08-20 00:31:56 +0300

    Updated projects

---
+++ CSharp.Essential/CSharp.Essential.Unit13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp.Essential.Unit13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Unit 13 tasks");
+            Console.WriteLine(new String('-', 50));
+
+//            Ex1Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex2Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex3Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex4Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex5Demo();
+            Console.ReadLine();
+            
+        }
+
+        public static void Ex1Demo()
+        {
+            Console.SetWindowSize(80, 40);
+
+            CSharp.Essential.Unit13.Task1.Matrix instance;
+            
+            for (int i = 0; i < 30; i++)
+            {
+                instance = new CSharp.Essential.Unit13.Task1.Matrix(i * 2);
+                new Thread(instance.Move).Start();
+            }
+
+
+            Console.ReadKey();
+        }
+
+        public static void Ex2Demo()
+        {
+            Console.SetWindowSize(80, 42);
+
+            CSharp.Essential.Unit13.Task2.Matrix instance;
+
+            for (int i = 0; i < 26; i++)
+            {
+                instance = new CSharp.Essential.Unit13.Task2.Matrix(i * 3, true);
+                new Thread(instance.Move).Start();
+            }
+        }
+    }
+}
---
+++ CSharp.Essential/CSharp.Essential.Unit14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp.Essential.Unit14
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Unit 14 tasks");
+            Console.WriteLine(new String('-', 50));
+
+//            Ex1Demo();
+            Console.WriteLine(new String('-', 50));
+            Ex2Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex3Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex4Demo();
+//            Console.WriteLine(new String('-', 50));
+//            Ex5Demo();
+            Console.ReadLine();
+        }
+
+        public static void Ex1Demo()
+        {
+            int[] nums = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14};
+
+            EvenNums en = new EvenNums(nums);
+            foreach (var num in en)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public static void Ex2Demo()
+        {
+            MyList<string> ml  = new MyList<string>();
+            ml.AddItem("Zero");
+            ml.AddItem("One");
+            ml.AddItem("Two");
+            ml.AddItem("Three");
+            ml.AddItem("Four");
+
+            Console.WriteLine("How many records: {0}", ml.GetItemsQty);
+
+            Console.WriteLine("Print all records:");
+            foreach (var item in ml)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("Print record 0: {0}", ml[0]);
+            Console.WriteLine("Print record 3: {0}", ml[3]);
+//            Console.WriteLine("Print record 6: {0}", ml[6]);
+
+        }
+    }
+}

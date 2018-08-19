c6c5cf26e11d48c3de34f9799cda878d689fe7af
holms <holms.html@gmail.com>
2018-08-20 00:30:37 +0300

    Finished Task2 on Unit14

---
+++ CSharp.Essential/CSharp.Essential.Unit14/Task1/EvenNums.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharp.Essential.Unit14
+{
+    public class EvenNums : IEnumerable
+    {
+        private int[] numbers;
+
+        public EvenNums(int[] nums)
+        {
+            numbers = nums;
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    yield return num;
+                }
+            }
+        }
+
+    }
+}
---
+++ CSharp.Essential/CSharp.Essential.Unit14/Task2/MyList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharp.Essential.Unit14
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private T[] items = new T[1];
+        private int pos = -1;
+
+        private bool ExtendItems()
+        {
+            T[] temp = new T[items.Length + 1];
+            for (int i = 0; i < items.Length; i++)
+            {
+                temp[i] = items[i];
+            }
+
+            this.items = temp;
+            if (items.Length == temp.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public T this[int index]
+        {
+            get {
+                if (index<=pos)
+                {
+                    return items[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public void AddItem(T item)
+        {
+            if (pos + 1 == items.Length)
+            {
+                ExtendItems();
+            }
+
+            items[++pos] = item;
+        }
+
+        public int GetItemsQty
+        {
+            get { return items.Length; }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+            
+//            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
